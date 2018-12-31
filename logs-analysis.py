a5b62d047f457c862bd1a2430910fcfebf3b8f11
Mark Dordel <dordelms@gmail.com>
2018-12-31 12:00:25 -0600

    Update logs-analysis.py

--- logs-analysis.py
+++ logs-analysis.py
@@ -12,6 +12,46 @@ def main():
     
     #queries go here
     
+    def popular_articles():
+    # Returns the top 3 most popular articles
+    connection, cursor = connect()
+
+    query = """
+
+            """
+    cursor.execute(query)
+    article_count = cursor.fetchall()
+    connection.close()
+        
+    return article_count
+    
+    def popular_authors():
+    # Returns a list of the most popular authors
+    connection, cursor = connect()
+
+    query = """
+
+            """
+    cursor.execute(query)
+    authors = cursor.fetchall()
+    connection.close()
+
+    return authors
+
+def error_days():
+    # Reports Days that have more then 1% errors
+    connection, cursor = connect()
+
+    query = """
+
+            """
+    
+    cursor.execute(query)
+    errors = cursor.fetchall()
+    connection.close()
+
+    return errors
+    
         # Close communication with the database
     cur.close()
     conn.close()
